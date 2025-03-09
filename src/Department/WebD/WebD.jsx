ec731b3d7dc4c426cd56d2a4ad630c26608b9188
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-03-10 04:03:33 +0530

    content-uploaded2

--- src/Department/WebD/WebD.jsx
+++ src/Department/WebD/WebD.jsx
@@ -469,20 +469,20 @@ const TextParallaxContentExample = () => {
       >
         <ExampleContent1 />
       </TextParallaxContent>
-      <TextParallaxContent
+      {/* <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Quality"
         heading="Never compromise."
       >
         <ExampleContent2 />
-      </TextParallaxContent>
-      <TextParallaxContent
+      </TextParallaxContent> */}
+      {/* <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Modern"
         heading="Dress for the best."
       >
         <ExampleContent3 />
-      </TextParallaxContent>
+      </TextParallaxContent> */}
     </div>
   );
 };
